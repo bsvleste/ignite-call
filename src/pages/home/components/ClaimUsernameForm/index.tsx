a48ab506d393ab6a75d4d76d8b76589f303562dc
Bruno <bvaleiro@gmail.com>
2023-03-14 20:30:16 -0300

    tipando formulario

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,13 +1,27 @@
 import { Button, TextInput } from "@ignite-ui/react";
 import { Form } from "./styles";
 import { ArrowRight } from 'phosphor-react'
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const claimUsernameFormSchema = z.object({
+  username: z.string(),
+})
+
+type ClaimUserformFormData = z.infer<typeof claimUsernameFormSchema>
 export default function ClaimUsernameform() {
+  const { register, handleSubmit } = useForm<ClaimUserformFormData>()
+
+  function handleClaimUsername(data: ClaimUserformFormData) {
+    console.log(data)
+  }
   return (
-    <Form as="form">
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
       <TextInput
         size='sm'
         prefix="ignite.com/"
         placeholder="seu usuario"
+        {...register('username')}
       />
       <Button size="sm" type="submit">
         Reservar
