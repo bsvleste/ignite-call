6ab04495d0548a789fb931af01911feb7602eb0c
Bruno <bvaleiro@gmail.com>
2023-03-15 12:38:59 -0300

    primeiro cadastro user

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -6,6 +6,7 @@ import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/router'
+import { api } from '../../lib/axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -40,7 +41,15 @@ export default function Register() {
     }
   }, [router.query?.username, setValue])
   async function handleRegister(data: RegisterFormData) {
-    console.log(data)
+    const { username, name } = data
+    try {
+      await api.post('/users', {
+        name,
+        username,
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
   return (
     <RegisterContainer>
