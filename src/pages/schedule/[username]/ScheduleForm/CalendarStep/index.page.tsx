e6bc2b1eb4dce46b085eb1013622fee5a5cced76
bsvleste <bvaleiro@gmail.com>
2023-03-30 16:44:28 -0300

    feature: timerpicker component

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.page.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.page.tsx
@@ -1,10 +1,28 @@
 import Calendar from "../../../../../components/Calendar";
-import { Container } from "./styles";
+import { Container, TimerPicker, TimerPickerHeader, TimerPickerItem, TimerPickerList } from "./styles";
 
 export default function CalendarStep() {
+    const isDateSelected = true
     return (
-        <Container>
+        <Container isTimerPickerOpen={isDateSelected}>
             <Calendar />
+            {isDateSelected && (
+                <TimerPicker>
+                    <TimerPickerHeader>terça feira <span>20 de setembro</span></TimerPickerHeader>
+                    <TimerPickerList >
+                        <TimerPickerItem>08:00</TimerPickerItem>
+                        <TimerPickerItem>08:00</TimerPickerItem>
+                        <TimerPickerItem>08:00</TimerPickerItem>
+                        <TimerPickerItem>08:00</TimerPickerItem>
+                        <TimerPickerItem disabled>08:00</TimerPickerItem>
+                        <TimerPickerItem>08:00</TimerPickerItem>
+                        <TimerPickerItem>08:00</TimerPickerItem>
+                        <TimerPickerItem>08:00</TimerPickerItem>
+                        <TimerPickerItem>08:00</TimerPickerItem>
+                        <TimerPickerItem>08:00</TimerPickerItem>
+                    </TimerPickerList>
+                </TimerPicker>
+            )}
         </Container>
     )
 }
